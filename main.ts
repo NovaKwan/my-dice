838eecf9735e1324e39853afdf1a0bbdd3f5f82d
NovaKwan <110724796+NovaKwan@users.noreply.github.com>
2022-08-06 21:01:38 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 input.onSound(DetectedSound.Loud, function () {
+    basic.showString("_")
     basic.showNumber(randint(1, 6))
 })
 input.onGesture(Gesture.Shake, function () {
+    basic.showString("-")
     basic.showNumber(randint(1, 6))
 })
 input.setSoundThreshold(SoundThreshold.Loud, 85)
